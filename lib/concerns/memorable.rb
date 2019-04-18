a6d08fee4bc67898eb1b800559395be81b1482ca
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-18 12:11:48 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,14 @@
 module Memorable
   module ClassMethods
   
-    def reset_all
+  def reset_all
     all.clear
   end
 
   def count
     all.count
   end
+ end
   
   module InstanceMethods
     
